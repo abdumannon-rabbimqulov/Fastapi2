31a62c118e357218ff72de01cc71ae5a82479e06
Abdumannon Rabbimqulov <abdumannonrab@icloud.com>
2026-04-16 14:33:45 +0500

    fastapi

--- users/router.py
+++ users/router.py
@@ -14,5 +14,15 @@ router=APIRouter(prefix='/user',tags=['auth'])
 @router.post("/sign-up")
 async def sign_up(user:SignUpSchema,db:AsyncSession=Depends(get_db)):
     db_username=db.execute(select(User)).where(User.username==user.username)
+    db_email=db.execute(select(User)).where(User.email==user.email)
+    if db_username:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail='bu username band')
+    if db_email:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail='bu email band')
+    user=User(**user.model_dump())
 
-    
+    await db.add(user)
+    await db.commit()
+    await db.refresh(user)
+
+
