9cd90c7681e31532dcc86a5c75a3353c30f0e4ff
Abdumannon Rabbimqulov <abdumannonrab@icloud.com>
2026-04-20 15:12:46 +0500

    fastapi

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from db import Base
+from sqlalchemy import Column, Integer, String, Text, Numeric, DateTime
+from sqlalchemy.sql import func
+
+
+class Products(Base):
+    id =Column(Integer,primary_key=True)
+    title = Column(String(100))
+    price=Column(Numeric(10,2),nullable=False)
+    desc=Column(Text)
+    created_at=Column(DateTime(timezone=True), server_default=func.now())
--- users/crud.py
+++ users/crud.py
@@ -20,7 +20,6 @@ async def login_crud(data:LoginSchema,db:AsyncSession):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="parol xato")
 
     token_data={"sub":str(db_user.id)}
-
     refresh_token=create_refresh_token(token_data)
     access_token=create_access_token(token_data)
 
@@ -33,4 +32,3 @@ async def login_crud(data:LoginSchema,db:AsyncSession):
 
     return response
 
-kjb'oboo opnpbn'
