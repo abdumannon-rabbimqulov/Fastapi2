1a6e3be85e849ccc394c69f524dab7e25074e296
Abdumannon Rabbimqulov <abdumannonrab@icloud.com>
2026-04-23 14:57:12 +0500

    fastapi

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from fastapi import FastAPI
 from users.models import User
 from db import engine, Base
 from users.router import router as user_router
-from Post.router import router as product_router
 
 app=FastAPI()
 app.include_router(user_router)
-app.include_router(product_router)
 
 
 @app.on_event("startup")
